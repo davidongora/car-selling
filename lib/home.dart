a5e42d48d1ab5870836ae69cda6393812beb3577
davidongora <davidongora@gmail.com>
2024-10-16 08:14:52 +0300

    fix: separation of concerns

---
+++ lib/home.dart
@@ -0,0 +1,32 @@
+import 'package:carselling/signup.dart';
+import 'package:flutter/material.dart';
+import 'package:carselling/login.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orange,
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      // body: const Center(
+      //   child: Text('Welcome to the Home Page'),
+      // ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const LoginPage()),
+              // MaterialPageRoute(builder: (context) => const Signup()),
+            );
+          },
+          child: const Text('Buy Now'),
+        ),
+      ),
+    );
+  }
+}
