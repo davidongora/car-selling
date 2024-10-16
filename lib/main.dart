af16640166c4fd30a02c062d6d5b1b8551cd232f
davidongora <davidongora@gmail.com>
2024-10-16 08:16:31 +0300

    fix: image from asswts folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-import 'package:carselling/login.dart';
+// import 'package:carselling/login.dart';
+import 'package:carselling/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -47,44 +48,16 @@ class _SplashScreenState extends State<SplashScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset('assets/logo.png', width: 100), // Add your logo here
+            Image.asset('assets/logo.jpeg', width: 100), // Add your logo here
             const SizedBox(height: 20),
             const Text(
-              'App Scaffold',
+              'CarSelling',
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 24,
               ),
             ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.orange,
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-      // body: const Center(
-      //   child: Text('Welcome to the Home Page'),
-      // ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const LoginPage()),
-            );
-          },
-          child: const Text('Buy Now'),
         ),
       ),
     );
